1dbd5ebed3d12de40f838c72b4e537a57b6b6dff
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-12-07 23:48:05 +0700

    feat: Added solution for 3rd exercise of 15th week.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BinaryTree<string> tree = new BinaryTree<string>();
+        int level = int.Parse(Console.ReadLine());
+        int nodeCount = (int)Math.Pow(2, level+1) - 1;
+
+        string inputNode;
+        while(nodeCount > 0)
+        {
+            inputNode = Console.ReadLine();
+            tree.Add(inputNode);
+            nodeCount--;
+        }
+
+        string searchNode = Console.ReadLine();
+        tree.GetAllOpponents(searchNode);
+    }
+}
